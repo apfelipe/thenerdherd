4d4b53d2c86d1e99e85e7a9dca712bd2e47b77ae
apfelipe <84399993+apfelipe@users.noreply.github.com>
2022-01-14 18:38:04 -0600

    site
    done

---
+++ dev/js/body.js
@@ -0,0 +1,7 @@
+// Include body scripts in the right order
+// They will be concatenated using gulp-rigger in body.min.js
+
+//= vendor/jquery.js
+//= lib/jquery.navkit.js
+//= lib/jquery.portfolio.js
+console.log("Hello World!")
